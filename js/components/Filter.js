fc7e668e7871c76832e05dd53d381546c25bb8f8
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-06-19 12:53:29 +0200

    feat:  add filter module

---
+++ js/components/Filter.js
@@ -0,0 +1,23 @@
+export default class Filter {
+	constructor(element, props) {
+		this.element = element;
+		this.render();
+	}
+	render() {
+		this.element.innerHTML = `
+        <div>
+        <p>
+        Search:
+        <input>
+      </p>
+      <p>
+        Sort by:
+        <select>
+          <option value="name">Alphabetical</option>
+          <option value="age">Newest</option>
+        </select>
+      </p>
+        </div>
+        `;
+	}
+}
